d143c7b8f05ca2d0e1866d0bf160868dc26f04cc
alisalali <alis.alali@hotmail.com>
2024-12-09 04:44:45 +0300

    03-alternative-route-definitions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/Home";
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  RouterProvider,
+  // Route,
+} from "react-router-dom";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
+
+// const router = createBrowserRouter(routeDefinitions);
+
 function App() {
   return <RouterProvider router={router} />;
 }
