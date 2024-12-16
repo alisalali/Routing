3a5bc0a71da89fc9640e36ae65dbe861536266c9
alisalali <alis.alali@hotmail.com>
2024-12-17 01:48:17 +0300

    07-using-navlinks

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,19 +1,37 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
 import classes from "./MainNavigation.module.css";
 
-export default function MainNavigation() {
+function MainNavigation() {
   return (
     <header className={classes.header}>
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to={"/"}>Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to={"/products"}>Products</Link>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
     </header>
   );
 }
+
+export default MainNavigation;
